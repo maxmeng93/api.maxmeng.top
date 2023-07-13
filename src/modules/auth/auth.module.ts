f6daa03e2d1a5801a4c4c6d2f52050575c0bfdcc
maxmeng93 <mengyucheng@yogorobot.com>
2023-07-13 17:04:29 +0800

    update

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { jwtConstants } from './constants';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -16,7 +18,13 @@ import { jwtConstants } from './constants';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    AuthService,
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -1,3 +1,8 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
 export const jwtConstants = {
   // jwt 密钥，不应该明文写在代码中，而是应该通过环境变量传入
   secret: '1234567890!@#$%^&*()',
