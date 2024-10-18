cc7a13f06b8f3313b607013c005a852145caaeb5
maxmeng93 <mengyucheng@yogorobot.com>
2024-10-18 17:31:32 +0800

    feat: 网格接口权限控制

--- src/modules/strategy/strategy.controller.ts
+++ src/modules/strategy/strategy.controller.ts
@@ -8,6 +8,7 @@ import {
   Request,
   Param,
   ParseIntPipe,
+  Query,
 } from '@nestjs/common';
 import {
   ApiOperation,
@@ -25,6 +26,7 @@ import {
 import { CreateStrategyTradeDto } from './dto/create-strategy-trade.dto';
 import { StrategyEntity } from './entity/strategy.entity';
 import { StrategyTradeEntity } from './entity/strategy-trade.entity';
+import { OnlyMaxRole } from 'src/constants';
 
 @Controller('strategiy')
 @ApiTags('strategiy')
@@ -40,22 +42,54 @@ export class StrategyController {
     @Request() req,
   ): Promise<Strategy> {
     const user: RequestUser = req.user;
-    return this.strategyService.create(user.userId, createStrategyDto);
+    return this.strategyService.create(user, createStrategyDto);
   }
 
-  @ApiOperation({ summary: '获取网格策略列表' })
-  @Get()
+  @ApiOperation({ summary: '获取当前用户的网格策略列表' })
+  @Get('my')
   @ApiOkResponse({ type: StrategyEntity, isArray: true })
-  findAll(@Request() req): Promise<Strategy[]> {
+  findMyGrids(@Request() req): Promise<Strategy[]> {
     const user: RequestUser = req.user;
-    return this.strategyService.findAll(user.userId);
+    return this.strategyService.findMyGrids(user);
   }
 
-  @ApiOperation({ summary: '获取当个网格策略详情' })
+  @ApiOperation({ summary: '获取所有网格策略' })
+  @Get()
+  @OnlyMaxRole()
+  @ApiOkResponse({ type: StrategyEntity, isArray: true })
+  async findAllGrids(
+    @Query('page', ParseIntPipe) page = 1,
+    @Query('pageSize', ParseIntPipe) pageSize = 10,
+    @Query('username') username,
+  ): Promise<{
+    list: Strategy[];
+    total: number;
+    page: number;
+    pageSize: number;
+  }> {
+    const { list, total } = await this.strategyService.findAllGrids({
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      where: username ? { User: { username: username } } : undefined,
+    });
+
+    return {
+      list,
+      total,
+      page: page,
+      pageSize: pageSize,
+    };
+  }
+
+  @ApiOperation({ summary: '获取单个网格策略详情' })
   @Get(':id')
   @ApiOkResponse({ type: StrategyEntity })
-  findOne(@Param('id', ParseIntPipe) id: number): Promise<Strategy> {
-    return this.strategyService.findOne(id);
+  findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Request() req,
+  ): Promise<Strategy> {
+    const user: RequestUser = req.user;
+    return this.strategyService.findOne(id, user);
   }
 
   @ApiOperation({ summary: '更新网格策略' })
@@ -65,14 +99,17 @@ export class StrategyController {
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateStrategyDto: UpdateStrategyDto,
+    @Request() req,
   ): Promise<Strategy> {
-    return this.strategyService.update(id, updateStrategyDto);
+    const user: RequestUser = req.user;
+    return this.strategyService.update(id, updateStrategyDto, user);
   }
 
   @ApiOperation({ summary: '删除网格策略' })
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number): Promise<null> {
-    return this.strategyService.remove(id);
+  remove(@Param('id', ParseIntPipe) id: number, @Request() req): Promise<null> {
+    const user: RequestUser = req.user;
+    return this.strategyService.remove(id, user);
   }
 
   @ApiOperation({ summary: '新增网格策略交易' })
@@ -82,8 +119,10 @@ export class StrategyController {
   createTrade(
     @Param('id', ParseIntPipe) id: number,
     @Body() data: CreateStrategyTradeDto,
+    @Request() req,
   ): Promise<StrategyTrade> {
-    return this.strategyService.createTrade(id, data);
+    const user: RequestUser = req.user;
+    return this.strategyService.createTrade(id, data, user);
   }
 
   @ApiOperation({ summary: '获取网格策略交易列表' })
@@ -91,14 +130,20 @@ export class StrategyController {
   @ApiOkResponse({ type: StrategyTradeEntity, isArray: true })
   findTradeByStrategyId(
     @Param('id', ParseIntPipe) id: number,
+    @Request() req,
   ): Promise<StrategyTrade[]> {
-    return this.strategyService.findTradeByStrategyId(id);
+    const user: RequestUser = req.user;
+    return this.strategyService.findTradeByStrategyId(id, user);
   }
 
   @ApiOperation({ summary: '删除网格策略交易' })
   @Delete('trade/:id')
-  removeTrade(@Param('id', ParseIntPipe) id: number): Promise<StrategyTrade> {
-    return this.strategyService.removeTrade(id);
+  removeTrade(
+    @Param('id', ParseIntPipe) id: number,
+    @Request() req,
+  ): Promise<StrategyTrade> {
+    const user: RequestUser = req.user;
+    return this.strategyService.removeTrade(id, user);
   }
 
   @ApiOperation({ summary: '修改网格策略交易' })
@@ -108,7 +153,9 @@ export class StrategyController {
   updateTrade(
     @Param('id', ParseIntPipe) id: number,
     @Body() data: CreateStrategyTradeDto,
+    @Request() req,
   ): Promise<StrategyTrade> {
-    return this.strategyService.updateTrade(id, data);
+    const user: RequestUser = req.user;
+    return this.strategyService.updateTrade(id, data, user);
   }
 }
